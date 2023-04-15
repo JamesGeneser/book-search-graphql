22503cfd147d4e6c08d67a11a7e3d457d5968d12
James Geneser <jamesgeneser@gmail.com>
2023-04-15 14:18:33 -0600

    .catch error added to connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "../.env" });
-mongoose.connect(
-  process.env.MONGO_DB_URI || "mongodb://127.0.0.1:27017/googlebooks",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose
+  .connect(
+    process.env.MONGO_DB_URI || "mongodb://127.0.0.1:27017/googlebooks",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(
+    console.log("Connected to mongodb atlas").catch((error) => {
+      console.log("error", error);
+    })
+  );
 
 module.exports = mongoose.connection;
