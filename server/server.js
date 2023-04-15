41d26ec128ed32bdcb1fa306bb319d00e07155a9
James Geneser <jamesgeneser@gmail.com>
2023-04-15 15:57:37 -0600

    graphql middleware addition to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const server = new ApolloServer({
+  context: authMiddleware,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +16,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
