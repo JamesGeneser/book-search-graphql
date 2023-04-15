b8297f81686af3ac8e61494036fd76600681bcec
James Geneser <jamesgeneser@gmail.com>
2023-04-15 16:08:05 -0600

    added typedefs and resolver reference in server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const { authMiddleware } = require("./utils/auth");
+
+const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const routes = require("./routes");
 
 const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+
   context: authMiddleware,
 });
 
@@ -22,11 +27,14 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-const startApolloServer = async () => {
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 };
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
