70f130572d7f19ab53a5cef62920b3a275338195
James Geneser <jamesgeneser@gmail.com>
2023-04-15 14:31:13 -0600

    MONGODB_URI renamed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "../.env" });
 mongoose
-  .connect(
-    process.env.MONGO_DB_URI || "mongodb://127.0.0.1:27017/googlebooks",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(
     console.log("Connected to mongodb atlas").catch((error) => {
       console.log("error", error);
