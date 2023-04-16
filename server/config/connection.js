298c9edba9c1c1abefafb894631d9225f5213f27
James Geneser <jamesgeneser@gmail.com>
2023-04-15 18:04:32 -0600

    trying a dirname file path convention

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: "../../.env" });
+const { path } = require("../models/Book");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks", {
     useNewUrlParser: true,
